5cfac7f61e3a4483ee65c53254e30df13711da07
Ayush Anand <anandayush983@gmail.com>
2026-03-14 17:06:24 +0530

    initial version push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+
+setTimeout(function(){
+
+document.getElementById("preloader").style.display = "none";
+document.getElementById("content").style.display = "block";
+
+},1000);
+
+});
